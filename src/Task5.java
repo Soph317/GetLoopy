95ecc29517187cacc52170494422b660f3eba6ff
soph317 <lefebvsr@mail.uc.edu>
2025-10-24 12:39:45 -0400

    Completed 5

---
+++ src/Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int a = 1; a <= i; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
