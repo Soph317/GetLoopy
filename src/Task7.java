1cfa0bdade3576f403e4eaeae16221b81e12a23f
soph317 <lefebvsr@mail.uc.edu>
2025-10-24 12:40:06 -0400

    Completed 7

---
+++ src/Task7.java
@@ -0,0 +1,10 @@
+public class Task7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int a = 1; a <= 5; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
